977b820227e10697e3b5e893034330ba0c284ce6
Patrick Trahan <34581132+patricktrahan@users.noreply.github.com>
2019-02-28 18:03:27 -0600

    Add files via upload

--- noscroll.js
+++ noscroll.js
@@ -1,1 +1,32 @@
-document.body.addEventListener('touchmove', function(e){ e.preventDefault(); });
+$("body").hammer().on('drag swipe', function(e){
+
+    var scrollTarget = $(e.gesture.target).closest(".scrollable");
+    if(scrollTarget.length)
+    {
+        var scrollTopMax = scrollTarget[0].scrollHeight - scrollTarget.outerHeight();
+        if(scrollTopMax > 0){
+            var scrollTop = scrollTarget.scrollTop();
+            if(scrollTop > 0 && scrollTop < scrollTopMax){
+                //console.log("scrolling in the middle");
+            }
+            else if(scrollTop <= 0 && e.gesture.deltaY < 0){
+                //console.log("scrolling from top");
+            }
+            else if(scrollTop >= scrollTopMax && e.gesture.deltaY > 0){
+                //console.log("scrolling from bottom");
+            }
+            else{
+                //console.log("trying to scroll out of boundaries");
+                e.gesture.preventDefault();
+            }
+        }
+        else{
+            //console.log("content to short to scroll");
+            e.gesture.preventDefault();
+        }
+    }
+    else{
+        //console.log("no containing element with class=scrollable");
+        e.gesture.preventDefault();
+    }
+});
